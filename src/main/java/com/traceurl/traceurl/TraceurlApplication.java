dd73137e904e7ee7dfed492a4eab57ccf402ae54
Jung Woon Jik <dnswlrsla@gmail.com>
2025-12-11 23:27:08 +0900

    feat: create Project

---
+++ src/main/java/com/traceurl/traceurl/TraceurlApplication.java
@@ -0,0 +1,13 @@
+package com.traceurl.traceurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traceurl/traceurl/TraceurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traceurl.traceurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
